9f355d9b36b76fbf849ec7469ace0529ed01a651
Viktor Bulvarenko <viktor@bulvarenko.pw>
2019-03-29 12:59:56 +0200

    init comit

--- src/controllers/quests.js
+++
@@ -1,7 +0,0 @@
-// module.exports.getAll = (req, res) => {};
-
-// module.exports.new = (req, res) => {};
-
-// module.exports.update = (req, res) => {};
-
-// module.exports.delete = (req, res) => {};
